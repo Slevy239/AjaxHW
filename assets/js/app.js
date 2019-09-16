8b6f37001c95fda78c607e13237fa68ee06d147a
Sam Levy <slevy239@gmail.com>
2019-09-16 13:11:51 -0400

    display gifs and buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-let buttons = ['phish', 'Grateful Dead', 'Philadelphia'];
+    var buttons = ['phish', 'Grateful Dead', 'Philadelphia'];
 
-// function displayGif() {
-    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    // buttons + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-    
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// })
-//     .then(function (response) {
-
-//         let results = response.data;
-//     })
-
-
-
-function displayGif() { 
-    $("#gifs").empty();
-    for (i = 0; i < buttons.length; i++) {
-                let btnTag = $("<button>");
-                btnTag.addClass("gif-btn");
-                btnTag.attr("data-name", buttons[i]);
-                btnTag.text(buttons[i]);
-                $("#buttons-view").append(btnTag);
-                console.log(buttons[i])
-            }
+    function displayButtons() {
+        $("#buttons-view").empty();
+        for (var i = 0; i <buttons.length; i++) {
+            var gifBtn = $("<button>");
+            gifBtn.addClass("button");
+            gifBtn.addClass("btn btn-primary");
+            gifBtn.attr("data-name", buttons[i]);
+            gifBtn.text(buttons[i]);
+            $("#buttons-view").append(gifBtn);
         }
+    }displayButtons()
 
-    $("#add-gif").on("click", function(event) {
-        event.preveentDefault();
-        let jif = $("#input").val().trim();
-        buttons.push(jif);
-        displayGif()
-        console.log(jif)
-    });
-    $(document).on("click", ".gif-btn", displayGif);
+    function newBtn() {
+        $("#add-gif").on("click", function() {
+            var button = $("#gif-input").val().trim();
+        })
+        displayButtons();
+    }newBtn()
 
-    displayGif();
+    function displayGif() {
+       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttons +"&api_key=tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL";
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function(response) {
+            console.log(response);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                gifDiv.addClass("gifDiv");
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.append(gifRating);
+
+                var gifImg = $("<img>");
+                gifImg.attr("src", results[i].images.fixed_height_small.url);
+                gifImg.addClass("image");
+                gifDiv.append(gifImg);
+                $("#gifs").append(gifDiv);
+
+            }
+        })
+    }
+    displayGif()
+
 })
