9fcf899724562d3ca78b3ff51a68b64ab13c79f2
Sam Levy <slevy239@gmail.com>
2019-09-17 01:07:47 -0400

    still can't get new buttons to show

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,58 @@
 $(document).ready(function () {
 
-    var buttons = ['phish', 'philadelphia'];
+    var buttons = ['phish', "coding"];
 
     function displayButtons() {
         $("#buttons-view").empty();
-        for (var i = 0; i <buttons.length; i++) {
+        for (var i = 0; i < buttons.length; i++) {
             var gifBtn = $("<button>");
             gifBtn.addClass("button");
-            gifBtn.addClass("btn btn-primary");
             gifBtn.attr("data-name", buttons[i]);
-            gifBtn.html(buttons[i]);
+            gifBtn.text(buttons[i]);
             $("#buttons-view").append(gifBtn);
-            console.log("display buttons");
         }
     }
 
-    
+    function addNewBtn() {
+        $("#add-gif").on("click", function (event) { //adds buttons to array
+            event.preventDefault();
+            var button = $("#gif-input").val();
+            buttons.push(button);
+            displayButtons();
+        })
+    }
+
+
     function displayGif() {
-       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttons +"&api_key=tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL";
-       console.log(queryURL);
-       $.ajax({
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttons + "&api_key=tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL";
+        console.log(queryURL);
+        $.ajax({
             url: queryURL,
             method: "GET"
         })
-        .done(function(response) {
-            console.log(response);
-            var results = response.data;
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
-                gifDiv.addClass("gifDiv");
-                var gifRating = $("<p>").text("Rating: " + results[i].rating);
-                gifDiv.append(gifRating);
-                
-                var gifImg = $("<img>");
-                gifImg.attr("src", results[i].images.fixed_height_small.url);
-                gifImg.addClass("image");
-                gifDiv.append(gifImg);
-                $("#gifs").append(gifDiv);
-                console.log("displayGif");
-                
-            }
-        })
-    }
-    $("#add-gif").on("click", function() { //adds buttons to array
-        event.preventDefault();
-        var button = $("#gif-input").val();
-        buttons.push(button);
-        console.log(buttons)
-        displayButtons();
-    })
-    displayButtons();
-    displayGif()
-    
-    // newBtn()
+            .done(function (response) {
+                var results = response.data;
 
-    // function searchClicked() {
-    //     $("#")
-    // }
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+                    gifDiv.addClass("gifDiv");
+                    var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                    gifDiv.append(gifRating);
+                    var gifImg = $("<img>");
+                    gifImg.attr("src", results[i].images.fixed_height_small.url);
+                    gifImg.addClass("image");
+                    gifDiv.append(gifImg);
+                    $("#gifs").append(gifDiv);
+                }
+            })
+    }
+
+
+    $(document).on("click", ".button", displayGif)
+
+
+    displayButtons();
+    displayGif();
+    addNewBtn();
 
 })
