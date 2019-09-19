2a6ee2429898f39c10f6d16adb0ae4d998816b73
Sam Levy <slevy239@gmail.com>
2019-09-18 20:09:19 -0400

    buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
 
-    var buttons = ['gif', 'computer', 'Eagles', 'Phillies', 'Flyers', 'Sixers', 'Embiid']; // buttons for gif searchs
+    var buttons = ['Eagles', 'Phillies', 'Flyers', 'Sixers', 'Embiid']; // buttons for gif searchs
     var gifNumber = 5; // results per search
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttons + "&limit=" + gifNumber + "&api_key=tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL";
 
+    
     function displayButtons() {
         $("#buttons-view").empty(); //clears id=buttons=view if there is anything previously there
         for (var i = 0; i < buttons.length; i++) {
@@ -31,6 +33,7 @@ $(document).ready(function () {
     function noInput() {
         alert("Enter Something!!!") //alerts user
     };
+    
 
     function displayGif() { // api query, with buttons array, limiting search results and API key
         var buttons = $(this).attr("data-name")
@@ -39,7 +42,8 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         })
-            .then(function (response) {
+        .then(function (response) {
+            console.log(response)
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>"); //creates new div for each gif
@@ -47,7 +51,7 @@ $(document).ready(function () {
                 var gifRating = $("<p>").text("Rating: " + results[i].rating); //dynamically adds paragraph tag with the gif's rating
                 gifDiv.prepend(gifRating); // adds gif rating to the div
                 var gifImg = $("<img>"); // creates img tag for each gif
-                gifImg.attr("src", results[i].images.fixed_height_small.url); // accesses the gif within the JSON object, in images
+                gifImg.attr("src", results[i].images.fixed_height.url); // accesses the gif within the JSON object, in images
                 gifImg.addClass("image"); //adds new class called image
                 gifDiv.append(gifImg); // adds the img tag to the div
                 $("#gifs").append(gifDiv); //takes id=gifs from HTML and appeds with the new div for each gif 
@@ -55,8 +59,7 @@ $(document).ready(function () {
         });
     };
 
-   
-    $(document).on("click", ".button", displayGif)  //allows for a new query once the dynamically added buttons are clicked
+   $(document).on("click", ".button", displayGif);
 
     displayButtons();
     displayGif();
