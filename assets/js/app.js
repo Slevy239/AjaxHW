337fbbca1a7f8cc0e8b1c2c1a4f27694041d3153
Sam Levy <slevy239@gmail.com>
2019-09-18 22:12:16 -0400

    rebuilt loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,10 @@ $(document).ready(function () {
 
     var buttons = ['Eagles', 'Phillies', 'Flyers', 'Sixers', 'Embiid']; // buttons for gif searchs
     var gifNumber = 5; // results per search
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttons + "&limit=" + gifNumber + "&api_key=tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL";
 
-    
+
     function displayButtons() {
-        $("#buttons-view").empty(); //clears id=buttons=view if there is anything previously there
+        $("#buttons-view").empty(); //clears id=buttons-view if there is anything previously there
         for (var i = 0; i < buttons.length; i++) {
             var gifBtn = $("<button>"); //variable to created button tag
             gifBtn.addClass("button");//adds button class
@@ -19,7 +18,8 @@ $(document).ready(function () {
 
     function addNewBtn() {
         $("#add-gif").on("click", function (event) { //click event for the "search a topic" button
-            event.preventDefault();
+            event.preventDefault()
+
             var button = $("#input").val(); //grabs value from the input
             buttons.push(button); //pushs the input into the array, creating a new button
             if (button === "") { //if input is blank, run noInput function
@@ -33,35 +33,33 @@ $(document).ready(function () {
     function noInput() {
         alert("Enter Something!!!") //alerts user
     };
-    
+
 
     function displayGif() { // api query, with buttons array, limiting search results and API key
-        event.preventDefault()
-        var buttons = $(this).attr("data-name")
+        var buttons = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttons + "&limit=" + gifNumber + "&api_key=tfZU03Q2B2QpE3Wn8OdWajo0JImv7arL";
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-        .then(function (response) {
-            console.log(response)
-            var results = response.data;
-            $("#gifs").empty()
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>"); //creates new div for each gif
-                gifDiv.addClass("gifDiv"); // gives each gif a class name
-                var gifRating = $("<p>").text("Rating: " + results[i].rating); //dynamically adds paragraph tag with the gif's rating
-                gifDiv.prepend(gifRating); // adds gif rating to the div
-                var gifImg = $("<img>"); // creates img tag for each gif
-                gifImg.attr("src", results[i].images.fixed_height.url); // accesses the gif within the JSON object, in images
-                gifImg.addClass("image"); //adds new class called image
-                gifDiv.append(gifImg); // adds the img tag to the div
-                $("#gifs").append(gifDiv); //takes id=gifs from HTML and appeds with the new div for each gif 
-            };
-        });
+            .then(function (response) {
+                var results = response.data;
+                for (var i = 0; i < results.length; i++) {
+
+                    var gif = $("<img>");
+                    var moving = results[i].images.original.url;
+
+                    gif.attr("src", moving);
+                    gif.attr("data-loop", moving);
+                    gif.attr("class", "gif-image");
+                    $("#gifs").append(gif);
+
+
+                };
+            });
     };
 
-   $(document).on("click", ".button", displayGif);
+    $(document).on("click", ".button", displayGif);
 
     displayButtons();
     displayGif();
